b2d6ccf11e9436306ed9b8c8299c6640c2e1b828
robtfm <50659922+robtfm@users.noreply.github.com>
2023-08-08 12:18:45 +0100

    tokio main

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
-use std::{env, sync::Arc};
+use std::env;
 
-use livekit::{Room, RoomOptions, DataPacketKind};
+use livekit::{DataPacketKind, Room, RoomOptions};
 
-fn main() {
-    let rt = Arc::new(
-        tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap(),
-    );
+#[tokio::main]
+async fn main() {
+    let url = env::var("LIVEKIT_URL").expect("LIVEKIT_URL is not set");
+    let token = env::var("LIVEKIT_TOKEN").expect("LIVEKIT_TOKEN is not set");
 
-    let task = rt.spawn(async move {
-        let url = env::var("LIVEKIT_URL").expect("LIVEKIT_URL is not set");
-        let token = env::var("LIVEKIT_TOKEN").expect("LIVEKIT_TOKEN is not set");
+    let (room, mut rx) = Room::connect(&url, &token, RoomOptions::default())
+        .await
+        .unwrap();
+    println!("Connected to room: {} - {}", room.name(), room.sid());
 
-        let (room, mut rx) = Room::connect(&url, &token, RoomOptions::default())
-            .await
-            .unwrap();
-        println!("Connected to room: {} - {}", room.name(), room.sid());
+    room.local_participant()
+        .publish_data(
+            "Hello world".to_owned().into_bytes(),
+            DataPacketKind::Reliable,
+            Default::default(),
+        )
+        .await
+        .unwrap();
 
-        room.local_participant()
-            .publish_data(
-                "Hello world".to_owned().into_bytes(),
-                DataPacketKind::Reliable,
-                Default::default(),
-            )
-            .await
-            .unwrap();
-
-        while let Some(msg) = rx.recv().await {
-            println!("Event: {:?}", msg);
-        }
-    });
-
-    rt.block_on(task).unwrap();
+    while let Some(msg) = rx.recv().await {
+        println!("Event: {:?}", msg);
+    }
 }
